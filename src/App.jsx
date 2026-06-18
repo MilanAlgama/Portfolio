fa56b526afeb497a19357893a62dd7bed2589cbf
MilanAlgama <milanharsha28@gmail.com>
2026-06-18 19:08:01 +0530

    Call the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="app">
-      <h1>My Portfolio</h1>
-      <p>Undergraduate at University of Sri Jayewardhanapura and ICBT campus </p>
+      <Navbar />
+
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
     </div>
   );
 }
