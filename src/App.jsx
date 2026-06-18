e15fadb40087aea146312b5d42bea4caf20c2c25
MilanAlgama <milanharsha28@gmail.com>
2026-06-18 14:24:20 +0530

    Initial portfolio Structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>My Portfolio</h1>
+      <p>Undergraduate at University of Sri Jayewardhanapura and ICBT campus </p>
+    </div>
+  );
+}
+
+export default App;
